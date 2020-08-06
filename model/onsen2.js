0f2f237e507f352310530af02789bfdbc6f9599e
tanjoin <tanjoin@users.noreply.github.com>
2020-08-07 04:21:24 +0900

    hide index

--- model/onsen2.js
+++ model/onsen2.js
@@ -65,9 +65,9 @@ module.exports = class Onsen {
         const content = radio.contents[j];
         await exec(`mkdir "output2/${radio.title}"`).catch(e => {});
         if (this.fileExists(`output2/${radio.title.replace(/\"/ig, '')}/${radio.title} ${content.title} ${content.filename}`)) {
-          console.log(`${i} Skip:       ${radio.title} ${content.title} ${content.streaming_url}`);
+          console.log(`Skip:       ${radio.title} ${content.title} ${content.streaming_url}`);
         } else {
-          console.log(`${i} Download:   ${radio.title} ${content.title} ${content.streaming_url}`);
+          console.log(`Download:   ${radio.title} ${content.title} ${content.streaming_url}`);
           await exec(`youtube-dl --o '${radio.title} ${content.title} ${content.filename.split(/\.(?=[^.]+$)/)[0]}.%(ext)s' ${content.streaming_url}`, {
             cwd: `output2/${radio.title.replace(/\"/ig, '')}`
           });  
